252853dec0dc1409d69cac1461de52946b2f8543
patrickmao <patrickmao93@gmail.com>
2024-03-11 16:05:19 +0800

    tweak stark config

--- hashes/starky_ctl/src/config.rs
+++ hashes/starky_ctl/src/config.rs
@@ -19,11 +19,11 @@ impl StarkConfig {
             security_bits: 100,
             num_challenges: 2,
             fri_config: FriConfig {
-                rate_bits: 1,
+                rate_bits: 3,
                 cap_height: 4,
                 proof_of_work_bits: 16,
                 reduction_strategy: FriReductionStrategy::ConstantArityBits(4, 5),
-                num_query_rounds: 84,
+                num_query_rounds: 28,
             },
         }
     }
